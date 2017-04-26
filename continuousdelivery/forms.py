6eb177c7409352aead8b6e8d46b096ea07376725
Cesar Cruz <cesar.analista@gmail.com>
2017-04-26 11:25:01 -0300

    block-ui

--- continuousdelivery/forms.py
+++ continuousdelivery/forms.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
 from django import forms
-import subprocess, tempfile, os, re
+import subprocess, tempfile, os, re, errno, shutil
 
 class DeployForm(forms.Form):
-    url_build_jenkins = forms.CharField(label='URL Build Jenkins')
-    ip_destination = forms.CharField(label='IP Destino')
+	url_build_jenkins = forms.CharField(label='URL Build Jenkins')
+	ip_destination = forms.CharField(label='IP Destino')
 
-    def execute(self):
-        print(self.cleaned_data['url_build_jenkins'])
-        print(self.cleaned_data['ip_destination'])
-
-        dirpath = tempfile.mkdtemp()
-        print(dirpath)
-        checkout_project(self.cleaned_data['url_build_jenkins'], dirpath)
-        compile_project(dirpath)
-        print(get_version_project(dirpath))
-
-        pass
+	def execute(self):
+		print(self.cleaned_data['url_build_jenkins'])
+		print(self.cleaned_data['ip_destination'])
+		try:
+			dirpath = tempfile.mkdtemp()
+			print(dirpath)
+			checkout_project(self.cleaned_data['url_build_jenkins'], dirpath)
+			compile_project(dirpath)
+			print(get_version_project(dirpath))
+		finally:
+			try:
+				shutil.rmtree(dirpath)
+			except OSError as exc:
+				if exc.errno != errno.ENOENT:
+					raise
 
 def checkout_project(url, dirpath):
 	subprocess.call('svn checkout {0} {1}'.format(url, dirpath) , shell=True)
@@ -25,6 +29,6 @@ def compile_project(dirpath):
 	subprocess.call('cd {0}; mvn install -Dmaven.test.skip=true'.format(dirpath), shell=True)
 
 def get_version_project(dirpath):
-    path_parametros = 'ERP-jar/src/main/java/logic/covabra/framework/parametros/ParametrosGlobaisERP.java'
-    conteudo_arquivo = open(os.path.join(dirpath, path_parametros)).read()
-    return re.search('[\d]+\.[\d]+\.[\d]+', conteudo_arquivo).group(0).replace('.', '_')
+	path_parametros = 'ERP-jar/src/main/java/logic/covabra/framework/parametros/ParametrosGlobaisERP.java'
+	conteudo_arquivo = open(os.path.join(dirpath, path_parametros)).read()
+	return re.search('[\d]+\.[\d]+\.[\d]+', conteudo_arquivo).group(0).replace('.', '_')

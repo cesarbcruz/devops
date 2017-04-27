42e41a54b7ec49e8fcd33b0e53534c885bd9cbd2
Cesar Cruz <cesar.analista@gmail.com>
2017-04-27 17:12:48 -0300

    layout

--- continuousdelivery/forms.py
+++ continuousdelivery/forms.py
@@ -1,34 +1,60 @@
 # -*- coding: utf-8 -*-
 from django import forms
-import subprocess, tempfile, os, re, errno, shutil
+import subprocess, tempfile, os, re, errno, shutil, logging
 
 class DeployForm(forms.Form):
-	url_project_repositorio = forms.CharField(label='URL Projeto Repositório')
-	ip_destination = forms.CharField(label='IP Destino')
+    url_project_repository = forms.CharField(label='URL Projeto Repositório')
+    ip_destination = forms.CharField(label='IP Destino')
 
-	def execute(self):
-		print(self.cleaned_data['url_project_repositorio'])
-		print(self.cleaned_data['ip_destination'])
-		try:
-			dirpath = tempfile.mkdtemp()
-			print(dirpath)
-			checkout_project(self.cleaned_data['url_project_repositorio'], dirpath)
-			compile_project(dirpath)
-			print(get_version_project(dirpath))
-		finally:
-			try:
-				shutil.rmtree(dirpath)
-			except OSError as exc:
-				if exc.errno != errno.ENOENT:
-					raise
+    def execute(self):
+        logger = create_log_deploy()
+        logger.info('URL project repository: {}'.format(self.cleaned_data['url_project_repository']))
+        logger.info('IP destination: {}'.format(self.cleaned_data['ip_destination']))
+        try:
+            dirpath = tempfile.mkdtemp()
+            logger.info('Create dir temp: {}'.format(dirpath))
+            checkout_project(self.cleaned_data['url_project_repository'], dirpath)
+            compile_project(dirpath)
+            version = get_version_project(dirpath)
+            create_tag_svn("TAG_VER_{}".format(version), self.cleaned_data['url_project_repository'], logger)
+            logger.info('Deploy finalizado')
+        except Exception as ex:
+            logger.error(ex, exc_info=True)
+        finally:
+            try:
+                shutil.rmtree(dirpath)
+            except OSError as exc:
+                if exc.errno != errno.ENOENT:
+                    raise
 
 def checkout_project(url, dirpath):
-	subprocess.call('svn checkout {0} {1}'.format(url, dirpath) , shell=True)
+    subprocess.call('svn checkout {0} {1}'.format(url, dirpath) , shell=True)
 
 def compile_project(dirpath):
-	subprocess.call('cd {0}; mvn install -Dmaven.test.skip=true'.format(dirpath), shell=True)
+    subprocess.call('cd {0}; mvn install -Dmaven.test.skip=true'.format(dirpath), shell=True)
 
 def get_version_project(dirpath):
-	path_parametros = 'ERP-jar/src/main/java/logic/covabra/framework/parametros/ParametrosGlobaisERP.java'
-	conteudo_arquivo = open(os.path.join(dirpath, path_parametros)).read()
-	return re.search('[\d]+\.[\d]+\.[\d]+', conteudo_arquivo).group(0).replace('.', '_')
+    path_parametros = 'ERP-jar/src/main/java/logic/covabra/framework/parametros/ParametrosGlobaisERP.java'
+    conteudo_arquivo = open(os.path.join(dirpath, path_parametros)).read()
+    return re.search('[\d]+\.[\d]+\.[\d]+', conteudo_arquivo).group(0).replace('.', '_')
+
+def create_tag_svn(version, url, logger):
+    url_tags = 'http://172.16.50.2/svn/repositorioCovabra/ERP/tags/'
+
+    tags = subprocess.check_output('svn ls {}'.format(url_tags), shell=True)
+    if bytes(version,"utf-8") in tags:
+        logger.info('Removendo TAG: {}'.format(version))
+        subprocess.call('svn delete {0}{1} -m "Removendo TAG Automaticamente"'.format(url_tags, version), shell=True)
+    logger.info('Criando TAG: {0} -> {1}{2}'.format(url, url_tags, version))
+    subprocess.call('svn copy {0} {1}{2} -m "Criando TAG Automaticamente"'.format(url, url_tags.replace('\n',''), version),shell=True)
+
+
+def create_log_deploy():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler('deploy.log', mode='w')
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
--- continuousdelivery/views.py
+++ continuousdelivery/views.py
@@ -13,4 +13,13 @@ class Deploy(FormView):
         return super(Deploy, self).form_valid(form)
 
 class SucessDeploy(TemplateView):
+
     template_name = "sucess_deploy.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(SucessDeploy, self).get_context_data(**kwargs)
+        log_file = open("deploy.log", "r")
+        log = log_file.read();
+        log_file.close()
+        context['log'] = log
+        return context

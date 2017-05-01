68f0a126ddb4287e1cdd3efc0793fcc7e47fbe0a
Cesar Boaventura Cruz <cesar.analista@gmail.com>
2017-04-30 23:10:31 -0300

    deploy remote jboss

--- continuousdelivery/forms.py
+++ continuousdelivery/forms.py
@@ -2,6 +2,8 @@
 from django import forms
 import subprocess, tempfile, os, re, errno, shutil, logging, smtplib
 from email.mime.text import MIMEText
+import pexpect
+from pexpect import pxssh
 
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
@@ -16,6 +18,9 @@ class DeployForm(forms.Form):
     ip_destination = forms.CharField(label='IP Destino')
 
     def execute(self):
+        user_svn = "cesar"
+        password_svn = "Herminia2"
+        jboss_home = "/opt/Packages/jboss-eap-6.1/"
         logger = create_log_deploy()
         logger.info('URL project repository: {}'.format(self.cleaned_data['url_project_repository']))
         logger.info('IP destination: {}'.format(self.cleaned_data['ip_destination']))
@@ -25,9 +30,11 @@ class DeployForm(forms.Form):
             checkout_project(self.cleaned_data['url_project_repository'], dirpath, logger)
             compile_project(dirpath, logger)
             version = get_version_project(dirpath)
-            tag = create_tag_svn("TAG_VER_{}".format(version), self.cleaned_data['url_project_repository'], logger)
-            folder_destination = "/usr/local/nova_versao/{}".format(version)
+            tag = create_tag_svn("TAG_VER_{}".format(version), self.cleaned_data['url_project_repository'], logger, user_svn, password_svn)
+            folder_destination = "/usr/local/nova_versao/{}/".format(version)
             send_email_notificaton(version, self.cleaned_data['ip_destination'], tag, folder_destination, logger)
+            binary_files = get_binary_files(dirpath)
+            send_binaries(self.cleaned_data['ip_destination'], binary_files, folder_destination, "root", "pa33Lx$k", logger, jboss_home)
         except Exception as ex:
             logger.error(ex, exc_info=True)
         finally:
@@ -50,15 +57,16 @@ def get_version_project(dirpath):
     conteudo_arquivo = open(os.path.join(dirpath, path_parametros)).read()
     return re.search('[\d]+\.[\d]+\.[\d]+', conteudo_arquivo).group(0).replace('.', '_')
 
-def create_tag_svn(version, url, logger):
+def create_tag_svn(version, url, logger, user_svn, password_svn):
     url_tags = 'http://172.16.50.2/svn/repositorioCovabra/ERP/tags/'
 
     tags = subprocess.check_output('svn ls {}'.format(url_tags), shell=True)
     if bytes(version,"utf-8") in tags:
         logger.info('Removing old tag: {}'.format(version))
-        subprocess.call('svn delete {0}{1} -m "Removendo TAG Automaticamente Sistema Deploy"'.format(url_tags, version), shell=True)
+        subprocess.call('svn delete {0}{1} -m "Removendo TAG Automaticamente Sistema Deploy" {2}'.format(url_tags, version, get_credentials_svn(user_svn, password_svn)), shell=True)
+
     logger.info('Create TAG: {0} -> {1}{2}'.format(url, url_tags, version))
-    subprocess.call('svn copy {0} {1}{2} -m "Criando TAG Automaticamente Sistema Deploy"'.format(url, url_tags.replace('\n',''), version),shell=True)
+    subprocess.call('svn copy {0} {1}{2} -m "Criando TAG Automaticamente Sistema Deploy" {3}'.format(url, url_tags.replace('\n',''), version, get_credentials_svn(user_svn, password_svn)),shell=True)
     return "{0}{1}".format(url_tags, version)
 
 
@@ -93,3 +101,45 @@ def send_email_notificaton(version, ip_destination, tag, folder_destination, log
     session.quit()
 
     logger.info('Send email notification : {}'.format(destinatario))
+
+def get_credentials_svn(user_svn, password_svn):
+    return " --non-interactive --trust-server-cert --username {0} --password {1}".format(user_svn, password_svn)
+
+def get_binary_files(dirpath):
+    return "{0}/ERP-jar/target/ERP-jar.jar " \
+           "{0}/ERP-jar/target/staging/dependency/ERP-ejb.jar " \
+           "{0}/ERP-ear/target/ERP-ear-1.0-SNAPSHOT.ear " \
+           "{0}/ERP-web/target/ERP-web.war " \
+        .format(dirpath)
+
+def send_binaries(hostname, binary_files, folder_destination, username, password, logger, jboss_home):
+    s = pxssh.pxssh()
+    s.login(hostname, username, password)
+
+    execute_command(s, "rm -rf {}".format(folder_destination), logger)
+    execute_command(s, "mkdir {}".format(folder_destination), logger)
+
+    var_command = "scp {0} {1}@{2}:{3}/.".format(binary_files, username, hostname, folder_destination)
+    var_child = pexpect.spawn(var_command)
+    i = var_child.expect(["password:", pexpect.EOF])
+
+    if i == 0:  # send password
+        var_child.sendline(password)
+        var_child.expect(pexpect.EOF, timeout=500)
+        logger.info("Binaries sent to {}".format(hostname))
+    elif i == 1:
+        logger.error("Got the key or connection timeout")
+
+    execute_command(s, "cp {0}{1} /usr/local/prj_ERP_Producao/.".format(folder_destination, "ERP-jar.jar"), logger)
+    execute_command(s, "cp {0}{1} /usr/local/prj_ERP_Producao/lib/.".format(folder_destination, "ERP-ejb.jar"), logger)
+    execute_command(s, "service jboss stop", logger)
+    execute_command(s, "cp {0}{1} {2}standalone/deployments/".format(folder_destination, "ERP-ear-1.0-SNAPSHOT.ear", jboss_home), logger)
+    execute_command(s, "cp {0}{1} {2}standalone/deployments/".format(folder_destination, "ERP-web.war", jboss_home), logger)
+    execute_command(s, "service jboss start", logger)
+
+    s.logout()
+
+def execute_command(s, cmd, logger):
+    s.sendline(cmd)
+    s.prompt()
+    logger.info(s.before)

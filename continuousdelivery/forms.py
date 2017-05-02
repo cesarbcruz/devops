4b298d66d72223a2ba4619b2c06b3c50ed24a0ee
Cesar Cruz <cesar.analista@gmail.com>
2017-05-02 10:28:18 -0300

    Organize parameters

--- continuousdelivery/forms.py
+++ continuousdelivery/forms.py
@@ -5,6 +5,7 @@ from email.mime.text import MIMEText
 import pexpect
 from pexpect import pxssh
 
+#param global
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
 sender = 'logicnotice@gmail.com'
@@ -12,6 +13,19 @@ pwd_email = 'pa33Lx$k'
 destinatario = 'cesar@logicsp.com.br'
 # 'michael.serafim@logicsp.com.br, roberto@logicsp.com.br, cesar@logicsp.com.br, ' \
 #'tadeu.santos@logicsp.com.br, daniel@logicsp.com.br'
+
+#param user
+user_svn = "cesar"
+password_svn = "Herminia2"
+
+#param project repository
+path_parametros = 'ERP-jar/src/main/java/logic/covabra/framework/parametros/ParametrosGlobaisERP.java'
+url_tags = 'http://172.16.50.2/svn/repositorioCovabra/ERP/tags/'
+
+#param server destination
+jboss_home = "/usr/local/jboss-eap-6.1/"
+erp_home = "/usr/local/prj_ERP_Producao/"
+folder_destination_base="/usr/local/nova_versao/{}/"
 
 partial = 0
 complete = 1
@@ -26,10 +40,7 @@ class DeployForm(forms.Form):
     type_deploy = forms.ChoiceField(label='Type deploy', widget=forms.RadioSelect, choices=TYPE_DEPLOY, initial=partial)
 
     def execute(self):
-        _start_time = time.time()
-        user_svn = "cesar"
-        password_svn = "Herminia2"
-        jboss_home = "/opt/Packages/jboss-eap-6.1/"
+        start_time = time.time()
         logger = create_log_deploy()
         logger.info('URL project repository: {}'.format(self.cleaned_data['url_project_repository']))
         logger.info('IP destination: {}'.format(self.cleaned_data['ip_destination']))
@@ -40,11 +51,11 @@ class DeployForm(forms.Form):
             compile_project(dirpath, logger)
             version = get_version_project(dirpath)
             tag = create_tag_svn("TAG_VER_{}".format(version), self.cleaned_data['url_project_repository'], logger, user_svn, password_svn)
-            folder_destination = "/usr/local/nova_versao/{}/".format(version)
+            folder_destination = folder_destination_base.format(version)
             send_email_notificaton(version, self.cleaned_data['ip_destination'], tag, folder_destination, logger)
             binary_files = get_binary_files(dirpath)
             send_binaries(self.cleaned_data['ip_destination'], binary_files, folder_destination, "root", "pa33Lx$k", logger, jboss_home, (self.cleaned_data['type_deploy']))
-            t_sec = round(time.time() - _start_time)
+            t_sec = round(time.time() - start_time)
             (t_min, t_sec) = divmod(t_sec, 60)
             (t_hour, t_min) = divmod(t_min, 60)
             logger.info('Time processing: {}hour:{}min:{}sec'.format(t_hour, t_min, t_sec))
@@ -66,13 +77,10 @@ def compile_project(dirpath, logger):
     logger.info('Compiled project')
 
 def get_version_project(dirpath):
-    path_parametros = 'ERP-jar/src/main/java/logic/covabra/framework/parametros/ParametrosGlobaisERP.java'
     conteudo_arquivo = open(os.path.join(dirpath, path_parametros)).read()
     return re.search('[\d]+\.[\d]+\.[\d]+', conteudo_arquivo).group(0).replace('.', '_')
 
 def create_tag_svn(version, url, logger, user_svn, password_svn):
-    url_tags = 'http://172.16.50.2/svn/repositorioCovabra/ERP/tags/'
-
     tags = subprocess.check_output('svn ls {}'.format(url_tags), shell=True)
     if bytes(version,"utf-8") in tags:
         logger.info('Removing old tag: {}'.format(version))
@@ -144,8 +152,8 @@ def send_binaries(hostname, binary_files, folder_destination, username, password
         logger.error("Got the key or connection timeout")
 
     if type_deploy == str(complete):
-        execute_command(s, "cp {0}{1} /usr/local/prj_ERP_Producao/.".format(folder_destination, "ERP-jar.jar"), logger)
-        execute_command(s, "cp {0}{1} /usr/local/prj_ERP_Producao/lib/.".format(folder_destination, "ERP-ejb.jar"), logger)
+        execute_command(s, "cp {0}{1} {2}.".format(folder_destination, "ERP-jar.jar", erp_home), logger)
+        execute_command(s, "cp {0}{1} {2}lib/.".format(folder_destination, "ERP-ejb.jar", erp_home), logger)
         execute_command(s, "service jboss stop", logger)
         execute_command(s, "cp {0}{1} {2}standalone/deployments/".format(folder_destination, "ERP-ear-1.0-SNAPSHOT.ear", jboss_home), logger)
         execute_command(s, "cp {0}{1} {2}standalone/deployments/".format(folder_destination, "ERP-web.war", jboss_home), logger)
